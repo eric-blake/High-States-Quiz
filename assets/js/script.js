21dc00e4bf136d2994ee01be26214cc27546cccb
eric-blake <>
2023-06-08 19:13:35 +0000

    Add playQuiz function

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+const playButton = document.getElementById('play-btn')
+
+
+playButton.addEventListener('click', functionplayQuiz)
+
+/**
+ * Play quiz function
+ */
+ functionplayQuiz() {
+
+}
+
+ 

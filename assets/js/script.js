6641f9b984cdd979b765c65eec17eaf8a12d1253
eric-blake <>
2023-06-08 19:49:16 +0000

    Add quiz questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,99 @@ playButton.addEventListener('click', playQuiz)
 }
 
  
+
+// Questions and answers
+
+const questions = [{
+    
+    question: "What state is know as The golden State",
+        answers: [{
+            text: "California",
+            correct: true
+        },
+        {
+            text: "Arizona",
+            correct: false
+        },
+        {
+            text: "Florida",
+            correct: false
+        },
+        {
+            text: "Hawaii",
+            correct: false
+        }
+    
+        ]
+    },
+    
+    {
+    question: 'What state is know as "The Grand Canyon State"',
+    answers: [{
+        text: "Arizona ",
+        correct: true
+    },
+    {
+        text: "Arkansas",
+        correct: false
+    },
+    {
+        text: "Alaska",
+        correct: false
+    },
+    {
+        text: "Alabama",
+        correct: false
+    }
+    
+    ]
+    },
+    
+    {
+    question: 'What state is know as "The Last Frontier state"',
+    answers: [{
+        text: "Alaska",
+        correct: true
+    },
+    {
+        text: "Hawaii",
+        correct: false
+    },
+    {
+        text: "Kentucky",
+        correct: false
+    },
+    {
+        text: "Missouri",
+        correct: false
+    }
+    
+    ]
+    },
+    
+    {
+    question: 'What state is know as "The Empire State"',
+    answers: [{
+        text: "New York",
+        correct: true
+    },
+    {
+        text: "Rhoe Island",
+        correct: false
+    },
+    {
+        text: "Tennessee",
+        correct: false
+    },
+    {
+        text: "Utah",
+        correct: false
+    },
+    
+    ]
+    },
+    ];
+
+
+
+
